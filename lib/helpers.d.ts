9a5b4422bad9dfd79b2eea736e9c35f81e4de243
Russell Small <Envian@users.noreply.github.com>
2018-05-16 02:44:54 -0700

    Refactored helpers class to use TS, updated folder structure.

---
+++ lib/helpers.d.ts
@@ -0,0 +1,6 @@
+import { ConfirmationOptions } from "atom";
+export declare function sleep(milliseconds: number): Promise<void>;
+export declare function mapby<K, V>(array: Array<V>, evaluator: (val: V) => K): Map<K, V>;
+export declare function groupby<K, V>(array: Array<V>, evaluator: (val: V) => K): Map<K, Array<V>>;
+export declare function confirm(options: ConfirmationOptions): Promise<number>;
+export declare function getError(error: any): any;
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,41 +1,42 @@
-"use strict";
 "use babel";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sleep(milliseconds) {
+/// <reference types="atom" />
+
+import { ConfirmationOptions } from "atom";
+
+export function sleep(milliseconds : number) : Promise<void> {
     return new Promise(resolve => {
         setTimeout(resolve, milliseconds);
     });
 }
-exports.sleep = sleep;
-function mapby(array, evaluator) {
+
+export function mapby<K, V>(array : Array<V>, evaluator : (val : V) => K) : Map<K, V> {
     return array.reduce((map, item) => {
         map.set(evaluator(item), item);
         return map;
-    }, new Map());
+    }, new Map<K, V>());
 }
-exports.mapby = mapby;
-function groupby(array, evaluator) {
+
+export function groupby<K, V>(array : Array<V>, evaluator : (val : V) => K) : Map<K, Array<V>> {
     return array.reduce((map, item) => {
         const val = evaluator(item);
         const array = map.get(val) || [];
         map.set(val, array);
         array.push(item);
         return map;
-    }, new Map());
+    }, new Map<K, Array<V>>());
 }
-exports.groupby = groupby;
-function confirm(options) {
+
+export function confirm(options : ConfirmationOptions) : Promise<number> {
     return new Promise(resolve => {
         atom.confirm(options, result => resolve(result));
     });
 }
-exports.confirm = confirm;
-function getError(error) {
-    if (typeof (error) === "string")
-        return error;
+
+// TODO: Deprecate this.
+export function getError(error : any) {
+    if (typeof(error) === "string") return error;
     if (error instanceof Array) {
         error = error[0];
     }
     return error.error_description || error.error || error.message || error;
 }
-exports.getError = getError;
